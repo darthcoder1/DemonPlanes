945433410481a8909ed618e67203a4b7a3de17f3
Martin Zielinski <martin@stupidmindlessfun.de>
2015-08-19 00:32:36 +0200

    first playercam + player controls iteration

---
+++ DemonPlane/Assets/CameraScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public float DampTime = 0.15f;
+	private GameObject[] Players;
+	private Vector3 CurrentCamVelocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Players = GameObject.FindGameObjectsWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 PlayerCenterPos = Vector3.zero;
+
+		foreach (GameObject Player in Players) 
+		{
+			PlayerCenterPos = Player.transform.position;
+		}
+
+		PlayerCenterPos /= Players.Length;
+		PlayerCenterPos.z = transform.position.z;
+
+		transform.position = Vector3.SmoothDamp(transform.position, PlayerCenterPos, ref CurrentCamVelocity, DampTime);
+	}
+}
---
+++ DemonPlane/Assets/ControllerScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerScript : MonoBehaviour {
+
+	private Vector3 Direction;
+	public float MinSpeed;
+	public float NormalSpeed;
+	public float MaxSpeed;
+	public float RotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+
+		Direction = new Vector3 (0, 1, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float hAxis = Input.GetAxis ("Horizontal");
+		float vAxis = Input.GetAxis ("Vertical");
+
+		// Rotation
+		float rotateBy = RotationSpeed * Time.deltaTime * hAxis;
+
+		Quaternion rotateByQuat = Quaternion.AngleAxis (rotateBy, Vector3.back);
+		Vector3 newDirection = rotateByQuat * Direction;
+
+		Vector3 position = transform.position;
+		Debug.DrawLine( position, position + Direction * 100, Color.red );
+
+		transform.rotation = rotateByQuat * transform.rotation;
+
+		float currentSpeed = NormalSpeed;
+		// Movement
+		if (vAxis < 0)
+		{
+			currentSpeed = Mathf.Lerp(MinSpeed, NormalSpeed,  1.0f + vAxis);
+		}
+		else if (vAxis > 0)
+		{
+			currentSpeed = Mathf.Lerp(NormalSpeed, MaxSpeed,  vAxis);
+		}
+
+		transform.position += Direction * currentSpeed * Time.deltaTime;
+
+		Direction = newDirection;
+
+	}
+}
