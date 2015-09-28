b536149acf14fcbc23f6553a1d0df257885ea639
freejsf <kardor@gmail.com>
2015-09-28 15:51:55 +0200

    hit fx get cleaned up

---
+++ DemonPlane/Assets/Scripts/DieAfterGivenTime.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DieAfterGivenTime : MonoBehaviour {
+
+	public float TimeToDie;
+
+	// Use this for initialization
+	void Start () {
+
+		Invoke ("Die", TimeToDie);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Die()
+	{
+		GameObject.Destroy(gameObject);
+	}
+}
